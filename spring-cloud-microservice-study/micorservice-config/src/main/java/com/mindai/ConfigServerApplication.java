5c9c77d464cf0f4d9e330e8f6514e7e0cec2946e
huangxiang <huangxiang@mindai.com>
2016-10-19 15:59:58 +0800

    spring-config

---
+++ spring-cloud-microservice-study/micorservice-config/src/main/java/com/mindai/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.mindai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Description:
+ * @Author: Huang Xiang
+ * @Date: 2016年10月17日
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
