fcff8aa1765b371820f4b05d96c654d8a45d8d18
huangxiang <huangxiang@mindai.com>
2016-10-19 15:57:11 +0800

    spring-register

---
+++ spring-cloud-microservice-study/microservice-register/src/main/java/com/mindai/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.mindai; /**
+ * @Description:
+ * @Author: Huang Xiang
+ * @Date: 2016年10月17日
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka做服务发现。
+ * @author eacdy
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
