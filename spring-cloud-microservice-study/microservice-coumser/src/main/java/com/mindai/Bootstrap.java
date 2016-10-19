b73f9da86fa2afcca00b0e3650eb58d72234541d
huangxiang <huangxiang@mindai.com>
2016-10-19 15:59:16 +0800

    spring-coumser

---
+++ spring-cloud-microservice-study/microservice-coumser/src/main/java/com/mindai/Bootstrap.java
@@ -0,0 +1,33 @@
+package com.mindai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Description:
+ * @Author: Huang Xiang
+ * @Date: 2016年10月17日
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+public class Bootstrap {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+//        SpringApplication.run(Bootstrap.class, args);
+        new SpringApplicationBuilder(Bootstrap.class).web(true).run(args);
+    }
+}
