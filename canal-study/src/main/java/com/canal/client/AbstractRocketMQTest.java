013691ad09b4739e2daf0e77aed7e12b81e1b8b3
huangxiang <huangxiang1@yy.com>
2018-11-10 15:43:32 +0800

    canal

---
+++ canal-study/src/main/java/com/canal/client/AbstractRocketMQTest.java
@@ -0,0 +1,9 @@
+package com.canal.client;
+
+public class AbstractRocketMQTest {
+
+    public static String topic       = "example";
+    public static String groupId     = "group";
+    public static String nameServers = "192.168.64.129:9876";
+
+}
